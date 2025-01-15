375fcc871fc5480b32b335b00e1113c5acb3acb7
Kautzi <alexanderkautz.ak@gmail.com>
2025-01-15 05:38:38 +0100

    NEXT 41

--- 02_Basics/2_3/While.cc
+++ 02_Basics/2_3/While.cc
@@ -3,16 +3,17 @@
 
 int main()
 {
-    std::int32_t sum = 0;
+    std::int32_t sum = 0U;
 
-    while (sum < 10)
+    while(sum < 10)
     {
-        std::cout << "\nCurrent Sum: " << sum << " Enter the next value: ";
-        std::int32_t input = 0;
+        std::cout << "\nCurrent Sum: " << sum << " Enter next value: ";
+        std::int32_t input = 0U;
         std::cin >> input;
 
         sum += input;
     }
+
 
     return 0;
 }
