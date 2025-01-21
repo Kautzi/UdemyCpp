c722c3ffbacf3e2c4acfc97a13b7426c491bde51
Kautzi <alexanderkautz.ak@gmail.com>
2025-01-21 05:51:38 +0100

    NEXT 52

--- 02_Basics/_Exercise1/Exercise/exercise.cc
+++ 02_Basics/_Exercise1/Exercise/exercise.cc
@@ -4,4 +4,19 @@
 
 void mod_cross_sum(int I, int J)
 {
+    for (int i = 0; i < I; i++)
+    {
+        for (int j = 0; j < J; j++)
+        {
+            std::cout << "I= " << i << ", " << "J= " << j << "=";
+            if((i+j) % 2 == 0 )
+            {
+                std::cout << "Gerade!" << std::endl;
+            }
+            else
+            {
+                std::cout << "Ungerade!" << std::endl;
+            }
+        }
+    }
 }
