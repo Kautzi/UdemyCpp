726fbbc137a387cff89e92d0fb128e0ae819fe2a
Kautzi <alexanderkautz.ak@gmail.com>
2025-01-23 05:53:45 +0100

    NEXT 57

--- 02_Basics/2_4/FunctionTemplate.cc
+++ 02_Basics/2_4/FunctionTemplate.cc
@@ -4,30 +4,34 @@
 /**
  * @brief Computes max of a and b
  *
- * @tparam T Should be int or double only!
+ * @tparam T should be int or double
  * @param a
  * @param b
  * @return
  */
+
 template <typename T>
-T max(T a, T b)
+T max(T a, T b) //to overload a finction the parameter have to be different
 {
     return a > b ? a : b;
 }
 
+
 int main()
 {
-    double da = 1.0;
-    double db = 3.0;
-    std::cout << max<double>(da, db) << '\n';
 
-    std::int32_t ia = 1;
-    std::int32_t ib = 3;
-    std::cout << max<double>(ia, ib) << '\n';
+    std::int32_t a = 2;
+    std::int32_t b = 6;
 
-    char ca = 'a';
-    char cb = 'b';
-    std::cout << max(ca, cb) << '\n';
+    double c = -1.34;
+    double d = 3.55;
+
+    char e = 'a';
+    char f = 'b';
+
+    std::cout << "The greater double is: " << max(d, c) << '\n';
+    std::cout << "The greater int is: " << max(b, a) << '\n';
+    std::cout << "The greater char is: " << max(e,f) << '\n';
 
     return 0;
 }
--- 02_Basics/2_4/Overload.cc
+++ 02_Basics/2_4/Overload.cc
@@ -1,25 +1,27 @@
 #include <cstdint>
 #include <iostream>
 
-double max(double a, double b)
+double max( double a, double b) //to overload a finction the parameter have to be different
 {
     return a > b ? a : b;
 }
 
-std::int32_t max(std::int32_t a, std::int32_t b)
+int max( int a, int b)
 {
     return a > b ? a : b;
 }
 
 int main()
 {
-    double da = 1.0;
-    double db = 3.0;
-    std::cout << max(da, db) << '\n';
 
-    std::int32_t ia = 1;
-    std::int32_t ib = 3;
-    std::cout << max(ia, ib) << '\n';
+    std::int32_t a = 2;
+    std::int32_t b = 6;
+
+    double c = -1.34;
+    double d = 3.55;
+
+    std::cout << "The greater double is: " << max(d, c) << '\n';
+    std::cout << "The greater int is: " << max(b,a) << '\n';
 
     return 0;
 }
