01e33275d366818dd2ecdbe6ac27ab0576192b08
Kautzi <alexanderkautz.ak@gmail.com>
2025-02-06 05:53:54 +0100

    NEXT 88

--- 03_ArrayPointer/3_2/Pointer1.cc
+++ 03_ArrayPointer/3_2/Pointer1.cc
@@ -5,13 +5,12 @@
 
 int main()
 {
-    int number = 5;
-    std::cout << "Value of number: " << number << '\n';
-    std::cout << "Address of number: " << &number << '\n'; // 3c
+    int num = 5;
+    int *p = &num;
 
-    int *p = &number;
-    std::cout << "Value of p: " << p << '\n';                              // 3c
-    std::cout << "Value of the mem addr that p points to: " << *p << '\n'; // 3c
+    std::cout << "Address of pointer ""p"": " << p << std::endl;
+    std::cout << "Value of Address ""p"" is pointing to: " <<*p << std::endl;
+    std::cout << "Address of pointer ""p"": " << &p << std::endl;
 
     return 0;
 }
