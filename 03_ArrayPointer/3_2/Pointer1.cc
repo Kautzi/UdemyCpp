b94667f48fafa3f3ccc70c8148d2ff07b64586c3
Kautzi <alexanderkautz.ak@gmail.com>
2025-02-07 05:54:45 +0100

    NEXT 91

--- 03_ArrayPointer/3_2/Pointer1.cc
+++ 03_ArrayPointer/3_2/Pointer1.cc
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <array>
+#include <cstdint>
 
 // &: Reference
 // *p: De-reference
 
 int main()
 {
-    int num = 5;
-    int *p = &num;
+    // int num = 5;
+    int *p = new int{4};
+    //*p = 4
 
-    std::cout << "Address of pointer ""p"": " << p << std::endl;
-    std::cout << "Value of Address ""p"" is pointing to: " <<*p << std::endl;
-    std::cout << "Address of pointer ""p"": " << &p << std::endl;
+    std::cout << "Address ehre pointer "
+                 "p"
+                 " is pointing to : "
+              << p << std::endl;
+    std::cout << "Value of Address "
+                 "p"
+                 " is pointing to: "
+              << *p << std::endl;
+    std::cout << "Address of pointer "
+                 "p"
+                 ": "
+              << &p << std::endl;
 
-    return 0;
+    delete(p);
+    //Create an array with 4 values
+
+    std::array<std::uint32_t, 4>arr_point={1, 2, 3, 4};
+
+    for (std::uint32_t i = 0; i < arr_point.size(); i++)
+    {
+        std::cout << (arr_point[i]) << std::endl;
+    }
+
+        return 0;
 }
