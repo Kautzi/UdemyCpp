7fa8a2f4c6a3cc8be17ba66e0d71c14a5a23d75f
Kautzi <alexanderkautz.ak@gmail.com>
2025-02-12 05:59:53 +0100

    NEXT Quiz 9

--- 03_ArrayPointer/_Exercise2/Exercise/main.cc
+++ 03_ArrayPointer/_Exercise2/Exercise/main.cc
@@ -1,32 +1,44 @@
+#include <array>
 #include <cstdint>
 #include <iostream>
 
-std::int32_t *array_constructor(const std::int32_t value,
-                                const std::size_t length)
+std::int32_t *array_constructor(const std::int32_t value, const std::size_t length)
 {
+
+    auto heap_array = new std::int32_t[length];
+    for (std::size_t i = 0; i < length; i++)
+    {
+        heap_array[i] = value;
+    }
+
+    return heap_array;
 }
+
+
+template <std::size_t N>
+std::int32_t *array_constructor_new(const std::int32_t value, const std::size_t length)
+{
+
+    auto heap_array = new std::int32_t[length];
+    for (std::size_t i = 0; i < length; i++)
+    {
+        heap_array[i] = value;
+    }
+
+    return heap_array;
+}
+
 
 int main()
 {
-    // Exercise
-    auto input_array_length = std::size_t{0};
-    auto input_array_value = std::int32_t{0};
 
-    std::cout << '\n' << "Enter the array length: " << '\n';
-    std::cin >> input_array_length;
-    std::cout << '\n' << "Enter the array value: " << '\n';
-    std::cin >> input_array_value;
-    std::cout << '\n';
+    auto my_array = array_constructor(2, 10);
 
-    auto *array2 = array_constructor(input_array_value, input_array_length);
-
-    for (std::size_t i = 0; i < input_array_length; i++)
+    for (std::size_t i = 0; i < 10; i++)
     {
-        std::cout << array2[i] << '\n';
+        std::cout << my_array[i] << std::endl;
     }
 
-    delete[] array2;
-    array2 = nullptr;
-
+    delete[] my_array;
     return 0;
 }
